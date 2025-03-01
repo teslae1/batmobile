5b39977d849b08616ca239713d69cd50a0e5f28a
Karl Mogensen <km@bleau.dk>
2025-03-01 15:48:53 +0100

    fixed bug: should prioritize complete dir search match before searching string contains

--- c.bat
+++ c.bat
@@ -2,10 +2,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Get the search arg
 set "searchStr=%1"
 
+REM get the list arg
 set "secondArg=%2"
 
+REM print help
 if "%searchStr%"=="?" (
 	echo __BATMOBILE__ 
 	echo --     "c mydir"     cd into the first directory with string contains match 
@@ -40,6 +43,17 @@ if "%secondArg%"=="" (
 			for /f "tokens=2 delims==" %%a in ('set %CD%%searchStr%') do cd %%a
 	        goto :EOF
 	)
+	REM first check for exact match - has highest priority
+	for /d %%D in (*) do (
+	    set "dirName=%%D"
+	    if  "!searchStr!" == "!dirName!" (
+	        set "found=1"
+	        endlocal
+	        cd %%D
+	        goto :EOF
+	    )
+	)
+	REM check for string contains match
 	for /d %%D in (*) do (
 	    set "dirName=%%D"
 	    if  not "!dirName:%searchStr%=!" == "!dirName!" (
@@ -48,7 +62,6 @@ if "%secondArg%"=="" (
 	        cd %%D
 	        goto :EOF
 	    )
-
 	)
 REM handle list choice
 ) else if "%secondArg%"=="-l" (
@@ -100,3 +113,5 @@ if "%found%"=="0" (
     echo No directory contains string '%searchStr%'
 )
 
+REM at the key currentdir + search - save the dir to cd
+REM when search and if direct (no l) check the variable
